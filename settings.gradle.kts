af0974ab3657d6490ae536d0271982994ecdedd8
theEvilReaper <steffenwx@gmail.com>
2025-03-22 22:01:50 +0100

    Migrate repository usage

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,44 +13,22 @@ plugins {
 }
 
 dependencyResolutionManagement {
-    if (System.getenv("CI") != null) {
-        repositoriesMode = RepositoriesMode.PREFER_SETTINGS
-        repositories {
-            maven("https://repo.htl-md.schule/repository/Gitlab-Runner/")
-            maven {
-                val groupdId = 28 // Gitlab Group
-                val ciApiv4Url = System.getenv("CI_API_V4_URL")
-                url = uri("$ciApiv4Url/groups/$groupdId/-/packages/maven")
-                name = "GitLab"
-                credentials(HttpHeaderCredentials::class.java) {
-                    name = "Job-Token"
-                    value = System.getenv("CI_JOB_TOKEN")
+    repositories {
+        maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven {
+            name = "OneLiteFeatherRepository"
+            url = uri("https://repo.onelitefeather.dev/onelitefeather")
+            if (System.getenv("CI") != null) {
+                credentials {
+                    username = System.getenv("ONELITEFEATHER_MAVEN_USERNAME")
+                    password = System.getenv("ONELITEFEATHER_MAVEN_PASSWORD")
                 }
+            } else {
+                credentials(PasswordCredentials::class)
                 authentication {
-                    create<HttpHeaderAuthentication>("header")
-                }
-            }
-        }
-    } else {
-        repositories {
-            mavenLocal()
-            mavenCentral()
-            maven { url = uri("https://repo.spring.io/milestone") }
-            maven { url = uri("https://repo.spring.io/snapshot") }
-            maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            //maven("https://reposilite.worldseed.online/public")
-            maven("https://jitpack.io")
-            maven {
-                val groupdId = 28 // Gitlab Group
-                url = uri("https://gitlab.onelitefeather.dev/api/v4/groups/$groupdId/-/packages/maven")
-                name = "GitLab"
-                credentials(HttpHeaderCredentials::class.java) {
-                    name = "Private-Token"
-                    val gitLabPrivateToken: String? by settings
-                    value = gitLabPrivateToken
-                }
-                authentication {
-                    create<HttpHeaderAuthentication>("header")
+                    create<BasicAuthentication>("basic")
                 }
             }
         }
@@ -58,11 +36,10 @@ dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
             version("micronaut", "4.4.4")
+            version("vulpes.model", "1.0.0")
 
-            library("vulpes.api", "net.theevilreaper.vulpes.api", "vulpes-spring-api").version("1.0.0-SNAPSHOT")
-            library("jetbrains.annotation", "org.jetbrains", "annotations").version("24.1.0")
-
-            library("jackson", "com.fasterxml.jackson.module", "jackson-module-kotlin").version("2.17.1")
+            library("vulpes.api", "net.theevilreaper.vulpes.api", "vulpes-model").versionRef("vulpes.model")
+            library("jetbrains.annotation", "org.jetbrains", "annotations").version("26.0.2")
 
             plugin("micronaut.application", "io.micronaut.application").versionRef("micronaut")
             plugin("micronaut.aot", "io.micronaut.aot").versionRef("micronaut")
